90b9f80be68b0486d035eac184d828c9d50d504b
MisterQuestions <misterquestions.mx@gmail.com>
2022-12-22 14:39:50 -0800

    chore: remove winston

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
 import { InterpolateActionInputDto } from './dtos/interpolate-action-input.dto';
-import winston from 'winston';
+import core from '@actions/core';
 
 async function run(): Promise<void> {
-  const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-      winston.format.label({ label: 'main' }),
-      winston.format.align(),
-      winston.format.timestamp(),
-      winston.format.printf((info) => `${info.timestamp} [${info.level}] ${info.message}`)
-    ),
-    transports: [new winston.transports.Console()],
-  });
-
   const input = new InterpolateActionInputDto();
-  logger.info(`input: ${JSON.stringify(input)}`);
+  core.info(`input: ${JSON.stringify(input)}`);
 }
 
 run();
