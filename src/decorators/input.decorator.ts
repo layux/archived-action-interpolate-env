e3838fc2b86ab19d3aa3caa592a522d283a83367
MisterQuestions <misterquestions.mx@gmail.com>
2022-12-24 18:02:49 -0600

    chore: update input decorator

--- src/decorators/input.decorator.ts
+++ src/decorators/input.decorator.ts
@@ -4,23 +4,16 @@ import * as core from '@actions/core';
 export const Input =
   (fieldName: string, required = false): PropertyDecorator =>
   (target, propertyKey) => {
-    const inputs = Reflect.getMetadata('action:inputs', target.constructor) || {};
     const input = core.getInput(fieldName, { required });
 
-    Reflect.defineMetadata(
-      'action:inputs',
-      {
-        ...inputs,
-        [propertyKey]: input,
-      },
-      target
-    );
+    Reflect.deleteProperty(target, propertyKey);
+    Reflect.defineProperty(target, propertyKey, {
+      value: input,
+      writable: true,
+      enumerable: true,
+      configurable: true,
+    });
 
-    core.debug(`this -> [${typeof this}] ${JSON.stringify(this)}`);
+    core.debug(`Input '${fieldName}' has value: '${input}'`);
     core.debug(`target -> [${target.constructor.name}] ${JSON.stringify(target)}`);
-    core.debug(`inputs -> [${typeof inputs}] ${JSON.stringify(inputs)}`);
-
-    Object.assign(target, { [propertyKey]: input });
-
-    core.debug(`Input ${fieldName} has value: ${input}, added to constructor inputs`);
   };
